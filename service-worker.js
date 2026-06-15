1f0f4d772c6bb644de2bbe9cf690cb3ced4bee6d
giamdocnhansukhanh-max <giamdocnhansukhanh@gmail.com>
2026-06-15 22:21:09 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,55 @@
+const CACHE_NAME = 'novaspeak-v1';
+const STATIC_ASSETS = [
+  '/novaspeak/',
+  '/novaspeak/index.html',
+  '/novaspeak/manifest.json',
+  '/novaspeak/icons/icon-192x192.png',
+  '/novaspeak/icons/icon-512x512.png'
+];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(STATIC_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  const url = new URL(request.url);
+  
+  if (!url.pathname.startsWith('/novaspeak/')) return;
+  if (request.method !== 'GET') return;
+  
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        if (response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
+        }
+        return response;
+      })
+      .catch(() => {
+        return caches.match(request).then((cached) => {
+          if (cached) return cached;
+          if (request.mode === 'navigate') {
+            return caches.match('/novaspeak/');
+          }
+        });
+      })
+  );
+});
